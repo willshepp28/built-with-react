e684a1a882ab8bfc9af045e1f56bad220cb6366c
willshepp28 <willsheppard29@gmail.com>
2023-10-17 18:17:41 -0400

    install date-fns; now displaying date in ios-bar-left

--- src/projects/instagram/ios-bar/ios-bar-left.tsx
+++ src/projects/instagram/ios-bar/ios-bar-left.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { format } from "date-fns";
 
 export default function IOSBarLeft() {
-  return <p>IosBarLeft</p>;
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 60000); // 60000 milliseconds = 1 minute
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formattedTime = format(currentTime, "h:mm a");
+
+  return <p>{formattedTime}</p>;
 }
