66f49de3193205ff93393ef48f7c85d49e89d337
willshepp28 <willsheppard29@gmail.com>
2023-10-19 09:15:20 -0400

    now using urls on cloudinary

--- src/projects/linkedin/my-network/my-network.tsx
+++ src/projects/linkedin/my-network/my-network.tsx
@@ -2,6 +2,14 @@ import styles from "../linkedin.module.css";
 import React, { useState } from "react";
 import { faker } from "@faker-js/faker";
 
+const userProfilePhotoUrl = [
+  'https://res.cloudinary.com/dfahwwvue/image/upload/v1697720696/linkedin-user-20_tnk5ln.webp',
+  'https://res.cloudinary.com/dfahwwvue/image/upload/v1697720696/linkedin-user-19_akcgpo.webp',
+  'https://res.cloudinary.com/dfahwwvue/image/upload/v1697720693/linkedin-user-2_lfrrfh.webp',
+  'https://res.cloudinary.com/dfahwwvue/image/upload/v1697720693/linkedin-user-3_mavkmg.webp',
+
+]
+
 export default function MyNetwork() {
   const columns = Array.from({ length: 16 }, (_, index) => index + 1);
 
@@ -15,14 +23,15 @@ export default function MyNetwork() {
         </div>
 
         <div className={`row mt-3 d-flex ${styles.userProfilesContainer}`}>
-          {columns.map((columnNumber) => (
+          {userProfilePhotoUrl.map((url) => (
             <div
               className={`${styles.userProfileCard}  ${styles.partialBgRow} col-3 mb-3`}
             >
               <div className="row d-flex justify-content-center mt-3">
                 <div className="col-12">
                   <img
-                    src="../../../../public/linkedin-user-1.png"
+                    // src="../../../../public/linkedin-user-1.png"
+                    src={url}
                     className={`img-fluid ${styles.linkedinCircle}`}
                   />
                 </div>
@@ -57,9 +66,9 @@ export default function MyNetwork() {
                     >
                       <path d="M9 4a3 3 0 11-3-3 3 3 0 013 3zM6.75 8h-1.5A2.25 2.25 0 003 10.25V15h6v-4.75A2.25 2.25 0 006.75 8zM13 8V6h-1v2h-2v1h2v2h1V9h2V8z"></path>
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" class="mercado-match" width="16" height="16" focusable="false">
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" class="mercado-match" width="16" height="16" focusable="false">
       <path d="M10.87 9.52a1 1 0 01-1.37.37l-2-1A1 1 0 017 8V5a1 1 0 012 0v2.42l1.5.74a1 1 0 01.37 1.36zM15 8a7 7 0 11-7-7 7 7 0 017 7zm-2 0a5 5 0 10-5 5 5 5 0 005-5z"></path>
-    </svg>
+    </svg> */}
                     <strong className={`${styles.ml3}`}>Connect</strong>
                   </button>
                 </div>
