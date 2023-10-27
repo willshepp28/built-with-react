6b3ccba365ea95e69f8e41bed9cfcac5286b0501
willshepp28 <willsheppard29@gmail.com>
2023-10-27 14:53:09 -0400

    updated instgram profile pic aspect ratio

--- src/projects/instagram/explore/explore-information-bar/profile-photo.tsx
+++ src/projects/instagram/explore/explore-information-bar/profile-photo.tsx
@@ -1,10 +1,13 @@
-import "./profile-photo.css";
+// import "./profile-photo.css";
+import styles from './profile-photo.module.css';
+
 import React from "react";
 
 export default function ProfilePhoto() {
   return (
     <div className="circle">
       <img
+        className={`${styles.circleImg}`}
         src="https://res.cloudinary.com/dfahwwvue/image/upload/v1697594796/bali-explore-1_uzgmu1.jpg"
         alt=""
       />
